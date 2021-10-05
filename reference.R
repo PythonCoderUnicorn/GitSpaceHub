e971a12a05c07a18a7d6ce4db7a00361511663aa
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2021-10-05 15:24:30 -0600

    Update reference.R

--- reference.R
+++ reference.R
@@ -25,6 +25,25 @@ df %>%
 
 
 
+
+#========== colors Usage in R base plots:
+barplot(1:5, col=rainbow(5))
+# Use heat.colors
+barplot(1:5, col=heat.colors(5))
+# Use terrain.colors
+barplot(1:5, col=terrain.colors(5))
+# Use topo.colors
+barplot(1:5, col=topo.colors(5))
+# Use cm.colors
+barplot(1:5, col=cm.colors(5))
+
+
+
+
+
+
+
+
 #-- ggplot 
 ggplot(data= mpg) +
   geom_point( aes(x= displ, y=hwy, size= class), color= "orange")
